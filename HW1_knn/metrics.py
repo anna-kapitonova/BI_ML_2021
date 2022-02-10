643b68ad13c804a460d9705168f5aedfc91f5f7b
Anna Kapitonova <48308650+anna-kapitonova@users.noreply.github.com>
2022-02-11 01:09:36 +0300

    uploaded files

---
+++ HW1_knn/metrics.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+
+def binary_classification_metrics(y_pred, y_true):
+    """
+    Computes metrics for binary classification
+    Arguments:
+    y_pred, np array (num_samples) - model predictions
+    y_true, np array (num_samples) - true labels
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    """
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+
+    TP = ((y_pred == 1) & (y_true == 1)).sum()
+    FP = ((y_pred == 1) & (y_true == 0)).sum()
+    FN = ((y_pred == 0) & (y_true == 1)).sum()
+
+    precision = TP / (TP+FP)
+    recall = TP / (TP+FN)
+    f1 = 2 * precision * recall / (precision + recall)
+    accuracy = (y_true == y_pred).sum() / len(y_true)
+
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(y_pred, y_true):
+    """
+    Computes metrics for multiclass classification
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true labels
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    """
+
+    accuracy = (y_true == y_pred).sum() / len(y_true)
+
+    return accuracy
+
+
+def r_squared(y_pred, y_true):
+    """
+    Computes r-squared for regression
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true values
+    Returns:
+    r2 - r-squared value
+    """
+
+    r2 = 1 - ((y_pred - y_true)**2).sum()/((y_true - np.mean(y_true))**2).sum()
+
+    return r2
+
+
+def mse(y_pred, y_true):
+    """
+    Computes mean squared error
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true values
+    Returns:
+    mse - mean squared error
+    """
+    
+    mse = (((y_pred - y_true)**2).sum())/len(y_true)
+
+    return mse
+
+
+def mae(y_pred, y_true):
+    """
+    Computes mean absolut error
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true values
+    Returns:
+    mae - mean absolut error
+    """
+
+    mae = (abs(y_true - y_pred).sum())/len(y_true)
+    
+    return mae
